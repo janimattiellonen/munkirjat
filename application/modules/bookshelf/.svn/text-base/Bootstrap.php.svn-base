f8b3fe909c96a6a66dd5c723d204dced3f061ea2
Janimatti Ellonen <janimatti.ellonen@gmail.com>
2012-02-02 23:30:10 +0100

    kk

--- application/modules/bookshelf/.svn/text-base/Bootstrap.php.svn-base
+++
@@ -1,5 +0,0 @@
-<?php
-class Bookshelf_Bootstrap extends Zend_Application_Module_Bootstrap
-{
-   
-}
--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-require_once dirname(__DIR__) . '/application/init.php';
-
-$application->run();
